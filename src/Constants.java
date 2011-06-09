b467395f9021c6b42a1f48218463867c992b877d
Marty Gilbert <martygilbert@gmail.com>
2011-06-09 16:11:21 -0400

    Added Constants.java

---
+++ src/Constants.java
@@ -0,0 +1,12 @@
+package gilbert.marty.util.qrcode;
+
+public class Constants {
+
+    public final static String FILE_FORMAT_SPECS = 
+        "<html>Input files must be CSV compliant text files, and must have the" +
+        "fields in the order given below for each of the different input " +
+        "file types.<br />" +
+        "<h2>Contacts</h2>\n"+
+        "</html>";
+
+}
